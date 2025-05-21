cb58c5a83c237d50e44b477cb279bb610ec9169d
Mahir Mohammed Labib <mahi.labib5@gmail.com>
2025-05-21 16:37:08 +0600

    Added to vercel

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const taskCollection = client.db("taskDB").collection("tasks");
 
@@ -34,7 +34,7 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -45,7 +45,7 @@ run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Tasks are getting ready!");
 });
 
 app.listen(port, () => {
